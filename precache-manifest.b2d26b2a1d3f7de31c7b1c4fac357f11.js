97dafb768828e74878be834ff87b7c115de4a83a
Maria Pensativa <mariya.pensativa@gmail.com>
2022-12-11 21:56:03 +0200

    Updates

---
+++ precache-manifest.b2d26b2a1d3f7de31c7b1c4fac357f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c628f206e2a4216625ba",
+    "url": "/react-shop/static/css/main.c6c12769.chunk.css"
+  },
+  {
+    "revision": "c628f206e2a4216625ba",
+    "url": "/react-shop/static/js/main.fce86ef3.chunk.js"
+  },
+  {
+    "revision": "9e829c67e1a7bf388c0c",
+    "url": "/react-shop/static/js/runtime~main.496f2218.js"
+  },
+  {
+    "revision": "7165849d109c079779ed",
+    "url": "/react-shop/static/js/2.38d239f8.chunk.js"
+  },
+  {
+    "revision": "c956c44e610dda302b69",
+    "url": "/react-shop/static/js/3.900cb80e.chunk.js"
+  },
+  {
+    "revision": "a0689585ce971d257d5001d591b5fe72",
+    "url": "/react-shop/index.html"
+  }
+];
